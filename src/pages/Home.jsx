533b0caf62bc181202111777f37e3c4c80941b81
giorgiopagani17 <giorgio.pagani2003@gmail.com>
2024-05-15 14:22:07 +0200

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from './elements/Navbar.jsx';
 import {
   Card,
-  CardHeader,
   CardBody,
-  Typography,
-  Avatar,
 } from "@material-tailwind/react";
-
-function StarIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="h-5 w-5 text-yellow-700"
-    >
-      <path
-        fillRule="evenodd"
-        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importa l'icona di Instagram
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import "./elements/Css/Home.css"; // Importa il file CSS per le animazioni
 
 const Home = () => {
+  const [typedText, setTypedText] = useState('');
+  const [index, setIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
+  const delay = 100; // Tempo di pausa tra ogni carattere (in millisecondi)
+  const fullText = "Full-Stack Developer";
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isTyping) {
+        if (index < fullText.length) {
+          setTypedText(prevText => prevText + fullText[index]);
+          setIndex(prevIndex => prevIndex + 1);
+        } else {
+          setIsTyping(false);
+          clearInterval(interval);
+          setTimeout(() => {
+            setIsTyping(false);
+            setIndex(0);
+            setTypedText('');
+            setIsTyping(true);
+          }, 1000); // Tempo di pausa prima di cancellare il testo (in millisecondi)
+        }
+      } else {
+        if (index > 0) {
+          setTypedText(prevText => prevText.slice(0, -1));
+          setIndex(prevIndex => prevIndex - 1);
+        } else {
+          setIsTyping(true);
+          clearInterval(interval);
+        }
+      }
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [index, isTyping]);
+
   return (
     <>
-      <Navbar/>
-
-      <Card color="transparent" shadow={false} className="w-1/2">
-      <CardBody className="mb-6 pl-20 text-white pt-20">
-        <strong className="text-white text-6xl">I'm Giorgio Pagani</strong>
-        <br/>
-        <strong className="text-[#fde047] text-5xl">Full-Stack Developer</strong>
-      </CardBody>
-    </Card>
+      <Navbar />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+        <Card color="transparent" shadow={false} className="w-1/2">
+          <CardBody className="mb-6 pl-5 text-white">
+            <strong className="text-white text-6xl">I'm Giorgio Pagani</strong>
+            <br />
+            <strong id="text" className="text-[#3b83bd] text-5xl">{typedText}</strong>
+            <p>Sono uno sviluppatore web</p>
+            <FontAwesomeIcon icon={faInstagram} beatFade size="2xl" style={{ color: "#ffffff", marginRight:'2%', cursor:'pointer' }} />
+            <FontAwesomeIcon icon={faLinkedin} beatFade size="2xl" style={{ color: "#ffffff", marginRight:'2%', cursor:'pointer' }} />
+            <FontAwesomeIcon icon={faGithub} beatFade size="2xl" style={{ color: "#ffffff", marginRight:'2%', cursor:'pointer' }} />
+            <FontAwesomeIcon icon={faEnvelope} beatFade size="2xl" style={{ color: "#ffffff", cursor:'pointer' }} />
+            <br/>
+            <button style={{backgroundColor:"#3b83bd", color:'white', borderRadius:'20px', padding: '0.7%'}}>Download Cv <FontAwesomeIcon icon={faDownload} bounce size="sm" style={{ color: "#ffffff"}}/></button>
+          </CardBody>
+        </Card>
+        <img src="web.png"/>
+      </div>
     </>
   );
 };
--- src/pages/elements/Navbar.jsx
+++ src/pages/elements/Navbar.jsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Navbar,
   MobileNav,
   Typography,
-  Button,
   IconButton,
-  Card,
 } from "@material-tailwind/react";
+import "./Css/Navbar.css"; // Importa il file CSS per le animazioni
 
 const Nav = () => {
-  const [openNav, setOpenNav] = React.useState(false);
- 
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false),
-    );
+  const [openNav, setOpenNav] = useState(false);
+  const [activeLink, setActiveLink] = useState("Home");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    setOpenNav(false);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) { // Imposta 768 come larghezza limite per il mobile
+      setOpenNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
- 
+
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-20">
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className={`nav-item ${activeLink === "Home" && "active"}`}
+        onClick={() => handleLinkClick("Home")}
       >
         <a href="#" className="flex items-center">
           Home
@@ -34,7 +44,8 @@ const Nav = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className={`nav-item ${activeLink === "About Me" && "active"}`}
+        onClick={() => handleLinkClick("About Me")}
       >
         <a href="#" className="flex items-center">
           About Me
@@ -44,7 +55,8 @@ const Nav = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className={`nav-item ${activeLink === "Progetti" && "active"}`}
+        onClick={() => handleLinkClick("Progetti")}
       >
         <a href="#" className="flex items-center">
           Progetti
@@ -54,7 +66,8 @@ const Nav = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className={`nav-item ${activeLink === "Skills" && "active"}`}
+        onClick={() => handleLinkClick("Skills")}
       >
         <a href="#" className="flex items-center">
           Skills
@@ -64,7 +77,8 @@ const Nav = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal"
+        className={`nav-item ${activeLink === "Contact" && "active"}`}
+        onClick={() => handleLinkClick("Contact")}
       >
         <a href="#" className="flex items-center">
           Contact
@@ -72,10 +86,10 @@ const Nav = () => {
       </Typography>
     </ul>
   );
- 
+
   return (
-    <div className="max-h-[768px] w-[100%]">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+    <div className="max-h-[768px] w-[100%] ">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none border-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Typography
             as="a"
@@ -86,15 +100,6 @@ const Nav = () => {
           </Typography>
           <div className="flex items-center gap-4">
             <div className="hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-              {/* <Button
-                variant="text"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Log In</span>
-              </Button> */}
-            </div>
             <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -105,7 +110,7 @@ const Nav = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  className="h-6 w-6"
+                  className="h-6 w-6 "
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                   strokeWidth={2}
