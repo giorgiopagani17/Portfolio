1fd8188a819d5e493ea9f90d03ca9de455de2fb5
giorgiopagani17 <giorgio.pagani2003@gmail.com>
2024-05-20 19:29:08 +0200

    Update Navbar.jsx

--- src/pages/elements/Navbar.jsx
+++ src/pages/elements/Navbar.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const [openNav, setOpenNav] = useState(false);
-  const [activeLink, setActiveLink] = useState("Home");
+  const [activeLink, setActiveLink] = useState("home");
   const navigate = useNavigate();
 
   const handleLinkClick = (link) => {
