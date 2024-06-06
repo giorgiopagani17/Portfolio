8d9af29c21a8a54c34acc1fd8261c94184379c21
Giorgio Pagani <163420490+giorgiopagani17@users.noreply.github.com>
2024-06-06 11:55:48 +0200

    ANIMATIONS

--- src/pages/About2.jsx
+++ src/pages/About2.jsx
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Card, CardBody } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
+import "intersection-observer"; // Importa il polyfill
+import "animate.css";
 import "./elements/Css/About.css";
 
-const Projects = () => {
+const About = () => {
   const navigate = useNavigate();
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+  const aboutMeRef = useRef(null);
+  const imageRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => {
@@ -15,6 +19,42 @@ const Projects = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !entry.target.dataset.animationApplied) {
+            entry.target.classList.add("animate__animated", "animate__zoomInDown");
+            entry.target.dataset.animationApplied = true;
+          } else if (!entry.isIntersecting && entry.target.dataset.animationApplied) {
+            entry.target.classList.remove("animate__animated", "animate__zoomInDown");
+            entry.target.dataset.animationApplied = false;
+          }
+        });
+      },
+      {
+        threshold: 0.3,
+      }
+    );
+  
+    if (aboutMeRef.current) {
+      observer.observe(aboutMeRef.current);
+    }
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+  
+    return () => {
+      if (aboutMeRef.current) {
+        observer.unobserve(aboutMeRef.current);
+      }
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, []);
+  
 
   const handleOpen = (url) => {
     window.open(url, "_blank");
@@ -45,7 +85,7 @@ const Projects = () => {
     </Card>
   );
 
-  const CardNumber = ({ imageUrl, title, description, onClick }) => (
+  const CardNumber = ({ title, description }) => (
     <Card className="relative flex items-center justify-center w-28 h-28 p-2 overflow-hidden border-2 border-transparent transition-colors duration-300 hover:border-[#3b83bd]">
       <div className="relative z-10 flex flex-col items-center">
         <strong className="pb-2 text-center text-lg text-[#3b83bd]">
@@ -59,138 +99,172 @@ const Projects = () => {
   return (
     <div className="min-h-screen bg-white">
       <div className="w-full overflow-hidden">
-        <svg id="svg" viewBox="0 0 1440 390" xmlns="http://www.w3.org/2000/svg" className="transition duration-300 ease-in-out delay-150 mt-[-15%]">
-          <path d="M 0,400 L 0,150 C 110.17857142857142,170.92857142857144 220.35714285714283,191.85714285714286 343,171 C 465.64285714285717,150.14285714285714 600.7500000000001,87.50000000000001 726,89 C 851.2499999999999,90.49999999999999 966.6428571428571,156.14285714285714 1084,177 C 1201.357142857143,197.85714285714286 1320.6785714285716,173.92857142857144 1440,150 L 1440,400 L 0,400 Z" stroke="none" stroke-width="0" fill="#0A0436" fill-opacity="1" className="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 200)"></path>
+        <svg
+          id="svg"
+          viewBox="0 0 1440 390"
+          xmlns="http://www.w3.org/2000/svg"
+          className="transition duration-300 ease-in-out delay-150 mt-[-15%]"
+        >
+          <path
+            d="M 0,400 L 0,150 C 110.17857142857142,170.92857142857144 220.35714285714283,191.85714285714286 343,171 C 465.64285714285717,150.14285714285714 600.7500000000001,87.50000000000001 726,89 C 851.2499999999999,90.49999999999999 966.6428571428571,156.14285714285714 1084,177 C 1201.357142857143,197.85714285714286 1320.6785714285716,173.92857142857144 1440,150 L 1440,400 L 0,400 Z"
+            stroke="none"
+            strokeWidth="0"
+            fill="#0A0436"
+            fillOpacity="1"
+            className="transition-all duration-300 ease-in-out delay-150 path-0"
+            transform="rotate(-180 720 200)"
+          ></path>
         </svg>
-      </div> 
+      </div>
       {isMobile ? (
-          <></>
-        ) : ( 
-          <div className="text-center text-[#3b83bd] text-6xl fade-in-scale mb-10">
-            <div className="relative flex flex-col justify-center items-center">
-              <div className="absolute mb-10 text-[#0A0436] opacity-25 blur-[3px] text-[125%] font-bold">
-                About Me
+        <></>
+      ) : (
+        <div className="text-center text-[#3b83bd] text-6xl mb-10">
+          <div className="relative flex flex-col justify-center items-center">
+            <div className="absolute mb-10 text-[#0A0436] opacity-25 blur-[3px] text-[125%] font-bold">
+              About Me
+            </div>
+            <div className="relative text-[#3b83bd] text-6xl font-bold">
+              About Me
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="flex justify-center items-center">
+        {isMobile ? (
+          <Card className="p-2 w-full">
+            <CardBody>
+              <div className="flex items-center justify-center">
+                <img
+                  src="../../fotocartoon.jpg"
+                  className="w-96 rounded-full"
+                  alt="Giorgio Pagani"
+                  ref={imageRef}
+                />
               </div>
-              <div className="relative text-[#3b83bd] text-6xl font-bold">
-                About Me
+              <div
+                className="flex flex-col justify-center"
+                ref={aboutMeRef}
+              >
+                <h1 className="mt-5 text-5xl text-[#3b83bd] flex items-center justify-center">
+                  <strong>About Me</strong>
+                </h1>
+                <div className="mt-5 text-lg flex items-center justify-center gap-1">
+                  <p className="w-1/2">
+                    Ciao👋🏼 Sono{" "}
+                    <strong className="text-[#3b83bd] inline-block whitespace-nowrap">
+                      Giorgio Pagani
+                    </strong>
+                    ! <br /> Un{" "}
+                    <strong className="text-[#3b83bd]">
+                      Full-Stack <span className="inline-block whitespace-nowrap">
+                        Developer👨🏻‍💻
+                      </span>
+                    </strong>{" "}
+                    <br /> situato a{" "}
+                    <strong className="text-[#3b83bd] inline-block whitespace-nowrap">
+                      Bergamo🌍
+                    </strong>
+                  </p>
+                  <p className="mt-2 flex items-center justify-center w-1/2">
+                    Sono un ragazzo creativo, educato e determinato. Ho un diploma in Sistemi
+                    Informativi Aziendali e ho ottime capacità di lavorare in un team.
+                  </p>
+                </div>
+                <hr className="mt-5 mb-5 h-1 bg-[#3b83bd]" />
+                <div className="grid grid-cols-2 gap-y-10 justify-center">
+                  <div className="flex justify-center">
+                    <InfoCard
+                      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcnZoKyWdFLJh6TNAlU4Y8QEAUVhISRa8J9g&s"
+                      title="Servizi Inf. Aziendali"
+                      description="Diploma"
+                      onClick={() =>
+                        handleOpen(
+                          "https://islotto.edu.it/indirizzo-di-studio/ite-sistemi-informativi-aziendali-sia/"
+                        )
+                      }
+                    />
+                  </div>
+                  <div className="flex justify-center">
+                    <InfoCard
+                      imageUrl="https://www.eventi-digitali.online/media/public/38/Logo-JAC.jpg"
+                      title="Web Development"
+                      description="Diploma ITS"
+                      onClick={() =>
+                        handleOpen("https://jac-its.it/corso/web-development/")
+                      }
+                    />
+                  </div>
+                  <div className="flex justify-center">
+                    <CardNumber title="7+" description="Linguaggi di Programmazione" />
+                  </div>
+                  <div className="flex justify-center">
+                    <CardNumber title="3" description="Progetti Completati" />
+                  </div>
+                </div>
+              </div>
+            </CardBody>
+          </Card>
+        ) : (
+          <div className="flex flex-col items-center">
+            <div className="flex">
+              <img
+                src="../../fotocartoon.jpg"
+                className="w-96 rounded-full mr-5"
+                alt="Giorgio Pagani"
+                ref={imageRef}
+              />
+              <div
+                className="ml-10 flex flex-col justify-center"
+                ref={aboutMeRef}
+              >
+                <div className="ml-5 mr-5 text-lg">
+                  <p className="text-xl">
+                    <p className="text-3xl mb-1">
+                      Ciao👋🏼<span className="ml-1"> Sono </span>
+                      <strong className="text-[#3b83bd]">Giorgio Pagani</strong>!
+                    </p>
+                    Un{" "}
+                    <strong className="text-[#3b83bd]">Full-Stack Developer</strong>
+                    👨🏻‍💻{" "}
+                    situato a <strong className="text-[#3b83bd]">Bergamo</strong>
+                    🌍
+                  </p>
+                  <p className="mt-2">
+                    Sono un ragazzo creativo, ambizioso e determinato. Ho un <br /> diploma in Sistemi Informativi
+                    Aziendali e ho ottime capacità <br /> di lavorare in un team.
+                  </p>
+                </div>
+                <hr className="mt-5 mb-5 h-1 bg-[#3b83bd]" />
+                <div className="flex flex-wrap justify-center gap-4">
+                  <InfoCard
+                    imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcnZoKyWdFLJh6TNAlU4Y8QEAUVhISRa8J9g&s"
+                    title="Servizi Inf. Aziendali"
+                    description="Diploma"
+                    onClick={() =>
+                      handleOpen(
+                        "https://islotto.edu.it/indirizzo-di-studio/ite-sistemi-informativi-aziendali-sia/"
+                      )
+                    }
+                  />
+                  <InfoCard
+                    imageUrl="https://www.eventi-digitali.online/media/public/38/Logo-JAC.jpg"
+                    title="Web Development"
+                    description="Diploma ITS"
+                    onClick={() =>
+                      handleOpen("https://jac-its.it/corso/web-development/")
+                    }
+                  />
+                  <CardNumber title="7+" description="Linguaggi di Programmazione" />
+                  <CardNumber title="3" description="Progetti Completati" />
+                </div>
               </div>
             </div>
           </div>
         )}
-        <div className="flex justify-center items-center" >
-            {isMobile ? (
-                <Card className="p-2 w-full">
-                  <CardBody>
-                      <div className="flex items-center justify-center">
-                      <img
-                          src="../../fotocartoon.jpg"
-                          className="w-96 rounded-full slide-in-left"
-                          alt="Giorgio Pagani"
-                      />
-                      </div>
-                      <div className="flex flex-col justify-center slide-in-right">
-                          <h1 className="mt-5 text-5xl text-[#3b83bd] flex items-center justify-center">
-                              <strong>About Me</strong>
-                          </h1>
-                          <div className="mt-5 text-lg flex items-center justify-center gap-1">
-                              <p className="w-1/2">
-                              Ciao👋🏼 Sono{" "}
-                              <strong className="text-[#3b83bd] inline-block whitespace-nowrap">Giorgio Pagani</strong>! <br /> Un{" "}
-                              <strong className="text-[#3b83bd]">Full-Stack <span className="inline-block whitespace-nowrap">Developer👨🏻‍💻</span></strong>{" "}
-                              <br/> situato a <strong className="text-[#3b83bd] inline-block whitespace-nowrap">Bergamo🌍</strong>
-                              </p>
-                              <p className="mt-2 flex items-center justify-center w-1/2">
-                              Sono un ragazzo creativo, educato e determinato. Ho un diploma in Sistemi
-                              Informativi Aziendali e ho ottime capacità di lavorare in un team.
-                              </p>
-                          </div>
-                          <hr className="mt-5 mb-5 h-1 bg-[#3b83bd]" />
-                          <div className="grid grid-cols-2 gap-y-10 justify-center">
-                              <div className="flex justify-center">
-                              <InfoCard 
-                                  imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcnZoKyWdFLJh6TNAlU4Y8QEAUVhISRa8J9g&s"
-                                  title="Servizi Inf. Aziendali"
-                                  description="Diploma"
-                                  onClick={() => handleOpen("https://islotto.edu.it/indirizzo-di-studio/ite-sistemi-informativi-aziendali-sia/")}
-                              />
-                              </div>
-                              <div className="flex justify-center">
-                              <InfoCard 
-                                  imageUrl="https://www.eventi-digitali.online/media/public/38/Logo-JAC.jpg"
-                                  title="Web Development"
-                                  description="Diploma ITS"
-                                  onClick={() => handleOpen("https://jac-its.it/corso/web-development/")}
-                              />
-                              </div>
-                              <div className="flex justify-center">
-                              <CardNumber 
-                                  title="7+"
-                                  description="Linguaggi di Programmazione"
-                              />
-                              </div>
-                              <div className="flex justify-center">
-                              <CardNumber 
-                                  title="3"
-                                  description="Progetti Completati"
-                              />
-                              </div>
-                          </div>
-                      </div>
-                  </CardBody>
-                </Card>
-            ) : (
-                <div className="flex flex-col items-center fade-in-scale">
-                    <div className="flex">
-                      <img
-                          src="../../fotocartoon.jpg"
-                          className="w-96 rounded-full slide-in-left mr-5"
-                          alt="Giorgio Pagani"
-                      />
-                      <div className="ml-10 flex flex-col justify-center slide-in-right">
-                          <div className="ml-5 mr-5 text-lg">
-                              <p className="text-xl">
-                                  <p className="text-3xl mb-1">
-                                      Ciao👋🏼<span className="ml-1"> Sono </span>
-                                      <strong className="text-[#3b83bd]">Giorgio Pagani</strong>!
-                                  </p>
-                                  Un{" "} <strong className="text-[#3b83bd]">Full-Stack Developer</strong>👨🏻‍💻{" "}
-                                  situato a <strong className="text-[#3b83bd]">Bergamo</strong>🌍
-                              </p>
-                              <p className="mt-2">
-                                  Sono un ragazzo creativo, ambizioso e determinato. Ho un <br /> diploma in Sistemi Informativi
-                                  Aziendali e ho ottime capacità <br /> di lavorare in un team.
-                              </p>
-                          </div>
-                          <hr className="mt-5 mb-5 h-1 bg-[#3b83bd]" />
-                          <div className="flex flex-wrap justify-center gap-4">
-                          <InfoCard 
-                              imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcnZoKyWdFLJh6TNAlU4Y8QEAUVhISRa8J9g&s"
-                              title="Servizi Inf. Aziendali"
-                              description="Diploma"
-                              onClick={() => handleOpen("https://islotto.edu.it/indirizzo-di-studio/ite-sistemi-informativi-aziendali-sia/")}
-                          />
-                          <InfoCard 
-                              imageUrl="https://www.eventi-digitali.online/media/public/38/Logo-JAC.jpg"
-                              title="Web Development"
-                              description="Diploma ITS"
-                              onClick={() => handleOpen("https://jac-its.it/corso/web-development/")}
-                          />
-                          <CardNumber 
-                              title="7+"
-                              description="Linguaggi di Programmazione"
-                          />
-                          <CardNumber
-                              title="3"
-                              description="Progetti Completati"
-                          />
-                          </div>
-                      </div>
-                    </div>
-                </div>
-            )}
-        </div>
+      </div>
     </div>
   );
 };
 
-export default Projects;
+export default About;
